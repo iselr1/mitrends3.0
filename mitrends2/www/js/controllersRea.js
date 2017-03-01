44e17b2879beb05ea360d1bec9621a4481243f35
sarahmele <sarah.mele@students.bfh.ch>
2017-03-01 12:17:53 +0100

    Gebaschtel bi de rea
    :)

--- mitrends2/www/js/controllersRea.js
+++ mitrends2/www/js/controllersRea.js
@@ -213,97 +213,32 @@ angular.module('starter.controllersRea', [])
     var popTitle = $translate.instant('INFO');
     var popTemplate = $translate.instant('SDTEMPLATE_POPUP');
 
+    var correct;
+    var incorrect;
+    var clickFrequency=0;
+    var counter = 0;
+    var results = [];
+    var lastTime;
+
     var alertPopup = $ionicPopup.alert({
       title: popTitle,
       template: popTemplate,
     });
     alertPopup.then(function() {
-      // Registrieren der Startzeit der Übung
-      var correct = 0;
-      var incorrect = 0;
-      var clickFrequency = 0;
-      var counter = 0;
-      var tempcorrect = 0;
-      var tempincorrect = 0;
-      var results = [];
-      var lastTime = (new Date()).getTime();
+      functcorrincorr();
+    });
+
+    // Function after alertPopup
+    functcorrincorr = function() {  // Registrieren der Startzeit der Übung
+      lastTime = (new Date()).getTime();
 
       // End excersise after 120 seconds
-      $interval(function() {
-        console.log(correct);
-        console.log(incorrect);
-        console.log("interval" + counter);
-        counter = counter + 1;
-        if (counter == 1) {
-          correct = SymDigService.getCorrect();
-          incorrect = SymDigService.getIncorrect();
-          clickFrequency = ((correct + incorrect) / ((SymDigService.getTimeExcersise() / 4) / 60000));
-          tempcorrect = correct;
-          tempincorrect = incorrect;
-        } else {
-          correct = SymDigService.getCorrect() - tempcorrect;
-          incorrect = SymDigService.getIncorrect() - tempincorrect;
-          clickFrequency = ((correct + incorrect) / ((SymDigService.getTimeExcersise() / 4) / 60000));
-          tempcorrect = correct;
-          tempincorrect = incorrect;
-        }
-        var partresult1 = {};
-        partresult1.name = "Anzahl korrekte Zuordnungen (während dem " + counter + ".Teil)";
-        partresult1.value = correct;
-        results.push(partresult1);
-        var partresult2 = {};
-        partresult2.name = "Anzahl inkorrekte Zuordnungen (während dem " + counter + ".Teil)";
-        partresult2.value = incorrect;
-        results.push(partresult2);
-        var partresult3 = {};
-        partresult3.name = "Klickfrequenz (während dem " + counter + ".Teil)";
-        partresult3.value = clickFrequency;
-        results.push(partresult3);
-        console.log("Zwischenresultate" + results);
-        if (counter == 4) {
-          $state.go('geschafftSD');
-          // Variables to store in the result file
-          var date = new Date();
-          var correct = SymDigService.getCorrect();
-          var incorrect = SymDigService.getIncorrect();
-          var clickFrequency = SymDigService.getClickFrequency();
-          var result1 = {};
-          result1.name = "Datum, Uhrzeit nach beenden der Übung";
-          result1.value = date.toString();
-          results.push(result1);
-          var result2 = {};
-          result2.name = "Anzahl korrekte Zuordnungen(insgesamt)";
-          result2.value = correct;
-          results.push(result2);
-          var result3 = {};
-          result3.name = "Anzahl inkorrekte Zuordnungen(insgesamt)";
-          result3.value = incorrect;
-          results.push(result3);
-          var result4 = {};
-          result4.name = "Klickfrequenz(insgesamt)";
-          result4.value = clickFrequency;
-          results.push(result4);
-          var result5 = {};
-          result5.name = "Dauer der Übung in Sekunden";
-          result5.value = (SymDigService.getTimeExcersise() / 1000);
-          results.push(result5);
-          console.log("FinalResultate" + results);
-          ExcersiseStorageService.saveResultsToFile("Zahl-Symbol Übung", results);
-        } else {
-          //do nothing
-        }
-      }, SymDigService.getTimeExcersise() / 4, 4);
-
-
-
-
+      $interval(functioninterval, SymDigService.getTimeExcersise() / 4, 4);
 
       // Fill the keyTable with the images in a random way and the numbers ordered from 1 to 9
       var ranNums = SymDigService.doShuffle([1, 2, 3, 4, 5, 6, 7, 8, 9]);
       console.log(ranNums);
       $scope.keyTable = SymDigService.fillKeyTable(ranNums);
-
-
 
       // Generate Tables with 18 random ordered images
       $scope.solveTable = SymDigService.fillSolveTable((SymDigService.genNums([1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9], 18)));
@@ -322,8 +257,8 @@ angular.module('starter.controllersRea', [])
       var solveTableOneComplete = false;
       $scope.setValueImage = function(digit) {
 
-        /*Funktion um die Längste Latenz zwischen zwei Klicks zu eruieren*/
-        var lastLatency = 0;
+      /*Funktion um die Längste Latenz zwischen zwei Klicks zu eruieren*/
+      var lastLatency = 0;
 
         function onClickCheck() {
           var timeNow = (new Date()).getTime();
@@ -333,7 +268,6 @@ angular.module('starter.controllersRea', [])
           } else {
             alert('Please wait at least 5 seconds between clicks!');
           }
-
           lastLatency = timeNow;
         }
 
@@ -398,9 +332,80 @@ angular.module('starter.controllersRea', [])
             //should not happen
           }
         }
-
       };
-    })
+    };
+
+    // Alles was im Intervall passiert
+    functioninterval = function() {
+      var tempcorrect = 0;
+      var tempincorrect = 0;
+      correct = 0;
+      incorrect = 0;
+
+      console.log(correct);
+      console.log(incorrect);
+      console.log("interval" + counter);
+      counter = counter + 1;
+      if (counter == 1) {
+        correct = SymDigService.getCorrect();
+        incorrect = SymDigService.getIncorrect();
+        clickFrequency = ((correct + incorrect) / ((SymDigService.getTimeExcersise() / 4) / 60000));
+        tempcorrect = correct;
+        tempincorrect = incorrect;
+      } else {
+        correct = SymDigService.getCorrect() - tempcorrect;
+        incorrect = SymDigService.getIncorrect() - tempincorrect;
+        clickFrequency = ((correct + incorrect) / ((SymDigService.getTimeExcersise() / 4) / 60000));
+        tempcorrect = correct;
+        tempincorrect = incorrect;
+      }
+      var partresult1 = {};
+      partresult1.name = "Anzahl korrekte Zuordnungen (während dem " + counter + ".Teil)";
+      partresult1.value = correct;
+      results.push(partresult1);
+      var partresult2 = {};
+      partresult2.name = "Anzahl inkorrekte Zuordnungen (während dem " + counter + ".Teil)";
+      partresult2.value = incorrect;
+      results.pus
+      h(partresult2);
+      var partresult3 = {};
+      partresult3.name = "Klickfrequenz (während dem " + counter + ".Teil)";
+      partresult3.value = clickFrequency;
+      results.push(partresult3);
+      console.log("Zwischenresultate" + results);
+      if (counter == 4) {
+        $state.go('geschafftSD');
+        // Variables to store in the result file
+        var date = new Date();
+        correct = SymDigService.getCorrect();
+        incorrect = SymDigService.getIncorrect();
+        clickFrequency = SymDigService.getClickFrequency();
+        var result1 = {};
+        result1.name = "Datum, Uhrzeit nach beenden der Übung";
+        result1.value = date.toString();
+        results.push(result1);
+        var result2 = {};
+        result2.name = "Anzahl korrekte Zuordnungen(insgesamt)";
+        result2.value = correct;
+        results.push(result2);
+        var result3 = {};
+        result3.name = "Anzahl inkorrekte Zuordnungen(insgesamt)";
+        result3.value = incorrect;
+        results.push(result3);
+        var result4 = {};
+        result4.name = "Klickfrequenz(insgesamt)";
+        result4.value = clickFrequency;
+        results.push(result4);
+        var result5 = {};
+        result5.name = "Dauer der Übung in Sekunden";
+        result5.value = (SymDigService.getTimeExcersise() / 1000);
+        results.push(result5);
+        console.log("FinalResultate" + results);
+        ExcersiseStorageService.saveResultsToFile("Zahl-Symbol Übung", results);
+      } else {
+        //do nothing
+      }
+    };
   })
 
   //--------------------------------------------------------//
