fde6996d5218958cfcd7163e116f66275e952b53
Alexandre Monterroso <alexandre.monterroso@bfh.ch>
2017-04-03 16:23:05 +0200

    lineTest add

--- mitrends3.0/www/js/linetest/LineTestCtrl.js
+++ mitrends3.0/www/js/linetest/LineTestCtrl.js
@@ -1,6 +1,6 @@
 angular.module('uszapp.linetest')
-        .controller('LineTestCtrl', ['$scope', '$state', 'LineTest', 'lineTests',
-            function ($scope, $state, LineTest, lineTests) {
+        .controller('LineTestCtrl', ['$scope', '$state', 'LineTest', 'lineTests', '$timeout',
+            function ($scope, $state, LineTest, lineTests, $timeout) {
 
                 var canvas = $('canvas.line-canvas')[0];
                 var currentCategoryIdx;  // the current index in the `lineTests` array
@@ -19,8 +19,11 @@ angular.module('uszapp.linetest')
                  */
                 function init() {
                     $scope.debug = false;
+                    $scope.showAnimation1 = true;
+                    $scope.showAnimation2 = false;
                     currentCategoryIdx = 0;  // the current index in the linetests array
                     $scope.showSummary = false; // if the summary screen should be shown
+                    $scope.showTest = false; // if the summary screen should be shown
                     $scope.canRestart = false; // if the line test can be restarted
                     $scope.canDoNext = false; // if the user can proceed to the next test
                     $scope.resultHistory = []; // saved results
@@ -30,9 +33,21 @@ angular.module('uszapp.linetest')
                     $scope.score = ''; // score for the current test
                     $scope.success = false; // if the test was successful
                     $scope.done = false; // if the test was finished (successfully or also if it failed)
-                    $scope.mouseTracker = new MouseTracker(canvas);
-                    loadTest(currentCategoryIdx);
-                    startTest();
+
+                    $scope.count = 0;
+                }
+
+                $scope.initTest = function () {
+                    $scope.showAnimation1 = false;
+                    $scope.showAnimation2 = false;
+                    $scope.showSummary = false;
+                    $scope.showTest = false;
+                    $timeout(function () {
+                        $scope.mouseTracker = new MouseTracker(canvas);
+                        loadTest(currentCategoryIdx);
+                        startTest();
+                        $scope.showTest = true;
+                    }, 200);
                 }
 
                 function loadTest(categoryIndex) {
@@ -48,6 +63,7 @@ angular.module('uszapp.linetest')
                     $scope.canDoNext = false;
 
                     lineTest.start().then(function (result) {
+                        $scope.count++;
                         $scope.done = true;
                         $scope.success = true;
                         $scope.score1 = result.score1;
@@ -56,6 +72,11 @@ angular.module('uszapp.linetest')
                         $scope.canRestart = false;
                         $scope.canDoNext = true;
                         $scope.allTestsFinished = currentCategoryIdx === lineTests.length - 1;
+                        if ($scope.count > 4) {
+                            result.test = 'LEFT';
+                        } else {
+                            result.test = 'RIGHT';
+                        }
                         if ($scope.allTestsFinished) {
                             $scope.endMessage = 'Weiter zur Zusammenfassung.';
                         } else {
@@ -72,14 +93,38 @@ angular.module('uszapp.linetest')
                         $scope.canDoNext = false;
                     });
                 }
+                $scope.beginLeft = function () {
+                    $scope.showAnimation1 = false;
+                    $scope.showAnimation2 = false;
+                    $scope.showSummary = false;
+                    $scope.showTest = false;
+                    $timeout(function () {
+                        currentCategoryIdx = 0;
+                        loadTest(currentCategoryIdx);
+                        startTest();
+                        $scope.showTest = true;
+                    }, 200);
+                    
 
+                }
                 $scope.nextTest = function () {
                     if ($scope.allTestsFinished) {
                         _($scope.resultHistory).each(function (res) {
                             res.canvas.style.width = '400px';
                             res.canvas.style.height = '300px';
                         });
-                        $scope.showSummary = true;
+                        if ($scope.count === lineTests.length) {
+                            $scope.showAnimation1 = false;
+                            $scope.showAnimation2 = true;
+                            $scope.showSummary = false;
+                            $scope.showTest = false;
+                        } else {
+                            $scope.showAnimation1 = false;
+                            $scope.showAnimation2 = false;
+                            $scope.showSummary = true;
+                            $scope.showTest = false;
+                        }
+
                     } else {
                         currentCategoryIdx += 1;
                         loadTest(currentCategoryIdx);
@@ -91,13 +136,6 @@ angular.module('uszapp.linetest')
                     loadTest(currentCategoryIdx);
                     startTest();
                 };
-
-
-                this.goToFirstRound = function() {
-                    $ionicHistory.clearHistory();
-                    $state.go('LineTest');
-                };
-
 
             }])
         .directive('injectElement', function () {
